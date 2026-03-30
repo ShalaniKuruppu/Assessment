4f1f293bf527c3580f43c10e408f630b347c0dde
shalani <shalanikuruppu08@gmail.com>
2026-03-30 13:37:14 +0530

    configure typeorm

--- mo-marketplace-api/src/app.module.ts
+++ mo-marketplace-api/src/app.module.ts
@@ -1,10 +1,24 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
 @Module({
-  imports: [],
+  imports: [
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      host: 'localhost',
+      port: 5433,
+      username: 'postgres',
+      password: 'password',
+      database: 'mo_marketplace',
+      autoLoadEntities: true,
+      synchronize: true,
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
+
+
 export class AppModule {}
